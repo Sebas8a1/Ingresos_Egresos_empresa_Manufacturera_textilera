a34702c5a250d36d4e4c72ab4691d3c719e209b1
FuryBaco <jarodriguez5699@misena.edu.co>
2022-08-25 12:26:51 -0500

    Creacion del esqueleto del proyecto en IntelliJ

---
+++ empresa_textil/src/main/java/com/los_super_master/empresa_textil/EmpresaTextilApplication.java
@@ -0,0 +1,13 @@
+package com.los_super_master.empresa_textil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpresaTextilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpresaTextilApplication.class, args);
+	}
+
+}
