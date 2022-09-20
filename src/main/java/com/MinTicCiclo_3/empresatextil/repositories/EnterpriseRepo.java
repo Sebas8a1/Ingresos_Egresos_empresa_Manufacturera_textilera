b8a3db81be5aac997140f1b71a1efa63675025fe
Sebas8a1 <111146914+Sebas8a1@users.noreply.github.com>
2022-09-20 01:08:16 -0500

    Delete src directory
    delete

--- src/main/java/com/MinTicCiclo_3/empresatextil/repositories/EnterpriseRepo.java
+++
@@ -1,9 +0,0 @@
-package com.MinTicCiclo_3.empresatextil.repositories;
-
-import com.MinTicCiclo_3.empresatextil.entity.Enterprise;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EnterpriseRepo extends CrudRepository<Enterprise,Long> {
-}
--- src/main/java/com/MinTicCiclo_3/empresatextil/repositories/ProfileRepo.java
+++
@@ -1,9 +0,0 @@
-package com.MinTicCiclo_3.empresatextil.repositories;
-
-import com.MinTicCiclo_3.empresatextil.entity.Profile;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProfileRepo extends CrudRepository<Profile,Long> {
-}
